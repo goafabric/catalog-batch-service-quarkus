17a62c2e24f70265058f00561326238a4c7be149
Andreas Mautsch <amautsch>
2026-03-31 16:11:57 +0200

    initial import

--- src/main/kotlin/org/goafabric/catalog/job/MyBatchlet.kt
+++ src/main/kotlin/org/goafabric/catalog/job/MyBatchlet.kt
@@ -2,9 +2,11 @@ package org.goafabric.catalog.job
 
 import jakarta.batch.api.AbstractBatchlet
 import jakarta.batch.runtime.BatchStatus
+import jakarta.enterprise.context.Dependent
 import jakarta.inject.Named
 
-@Named
+@Named("myBatchlet")
+@Dependent
 class MyBatchlet : AbstractBatchlet() {
     override fun process(): String {
         println("yolo")
