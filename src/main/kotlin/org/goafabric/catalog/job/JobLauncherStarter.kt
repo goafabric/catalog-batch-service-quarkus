95303d1ea691fef904e93f41381bdee3160faf18
Andreas Mautsch <amautsch>
2026-05-20 07:37:57 +0200

    chang

--- src/main/kotlin/org/goafabric/catalog/job/JobLauncherStarter.kt
+++ src/main/kotlin/org/goafabric/catalog/job/JobLauncherStarter.kt
@@ -1,23 +1,21 @@
 package org.goafabric.catalog.job
 
+import io.quarkus.arc.All
 import io.quarkus.runtime.StartupEvent
 import jakarta.batch.operations.JobOperator
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.enterprise.event.Observes
+import org.jberet.job.model.Job
 import java.util.*
 
 @ApplicationScoped
-class JobLauncherStarter(private val jobOperator: JobOperator) {
+class JobLauncherStarter(private val jobOperator: JobOperator, @param:All private val jobs: MutableList<Job>) {
 
     fun onStart(@Observes ev: StartupEvent) {
         run()
     }
 
     fun run() {
-        //jobOperator.start("simpleItemJob", Properties())
-        jobOperator.start("chargeItemJob", Properties())
-        jobOperator.start("conditionJob", Properties())
-        jobOperator.start("insuranceJob", Properties())
-
+        jobs.forEach { job -> jobOperator.start(job.id, Properties()) }
     }
 }
