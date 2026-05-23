9961d33b4d2ddcaec7de2eabc65f9ef335cd0686
Andreas Mautsch <amautsch>
2026-05-23 09:55:55 +0200

    chang

--- src/main/kotlin/org/goafabric/catalog/Application.kt
+++ src/main/kotlin/org/goafabric/catalog/Application.kt
@@ -3,9 +3,12 @@ package org.goafabric.catalog
 import io.quarkus.runtime.Quarkus
 import io.quarkus.runtime.annotations.QuarkusMain
 import io.quarkus.runtime.annotations.RegisterForReflection
+import org.goafabric.catalog.job.chargeitem.ChargeItemEo
+import org.goafabric.catalog.job.condition.ConditionEo
+import org.goafabric.catalog.job.insurance.InsuranceEo
 
 @QuarkusMain
-@RegisterForReflection(targets = [IllegalArgumentException::class])
+@RegisterForReflection(targets = [IllegalArgumentException::class, InsuranceEo::class, ChargeItemEo::class, ConditionEo::class])
 class Application
 fun main(args: Array<String>) {
     Quarkus.run(*args)
