ffb8f98192e02bad7e0406593250aac44d0cac6e
Andreas Mautsch <amautsch>
2026-05-23 09:42:26 +0200

    chang

--- src/main/kotlin/org/goafabric/catalog/Application.kt
+++ src/main/kotlin/org/goafabric/catalog/Application.kt
@@ -2,8 +2,10 @@ package org.goafabric.catalog
 
 import io.quarkus.runtime.Quarkus
 import io.quarkus.runtime.annotations.QuarkusMain
+import io.quarkus.runtime.annotations.RegisterForReflection
 
 @QuarkusMain
+@RegisterForReflection(targets = [IllegalArgumentException::class])
 class Application
 fun main(args: Array<String>) {
     Quarkus.run(*args)
