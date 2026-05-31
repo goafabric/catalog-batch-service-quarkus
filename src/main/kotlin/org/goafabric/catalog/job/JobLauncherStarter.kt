742cad5223412991e0185f0235a49468c3f3e73f
Andreas Mautsch <amautsch>
2026-05-31 09:49:17 +0200

    chang

--- src/main/kotlin/org/goafabric/catalog/job/JobLauncherStarter.kt
+++ src/main/kotlin/org/goafabric/catalog/job/JobLauncherStarter.kt
@@ -6,6 +6,7 @@ import jakarta.batch.operations.JobOperator
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.enterprise.event.Observes
 import org.eclipse.microprofile.config.inject.ConfigProperty
+import org.goafabric.catalog.job.condition.ConditionRepository
 import org.jberet.job.model.Job
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -15,7 +16,8 @@ import kotlin.system.exitProcess
 @ApplicationScoped
 class JobLauncherStarter(private val jobOperator: JobOperator,
            @param:All private val jobs: MutableList<Job>,
-           @param:ConfigProperty(name = "database.provisioning.goals") private val goals: String) {
+           @param:ConfigProperty(name = "database.provisioning.goals") private val goals: String,
+           private val conditionRepository: ConditionRepository) {
 
     private val log: Logger = LoggerFactory.getLogger(this.javaClass)
 
@@ -26,8 +28,15 @@ class JobLauncherStarter(private val jobOperator: JobOperator,
     fun run() {
         if (goals.contains("-import-catalog-data")) {
             jobs.forEach {
-                job -> jobOperator.start(job.id, Properties())
-                log.info("starting job {}", job.id)
+                job ->
+                run {
+                    if (conditionRepository.findByDisplayStartsWith("").isEmpty()) { //jobOperator.getJobInstances(job.id, 0, 1).isEmpty()) {
+                        jobOperator.start(job.id, Properties())
+                        log.info("starting job {}", job.id)
+                    } else {
+                        log.info("skipping job {}", job.id)
+                    }
+                }
             }
         }
 
